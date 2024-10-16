78dbf26c9c105739afe14f1638e125d2d2546891
Aztro-dev <davidperezcastellanos@gmail.com>
2024-10-16 10:50:05 -0500

    assign zero register to always be zero

--- DecodeExecute/regfile.v
+++ DecodeExecute/regfile.v
@@ -1,21 +1,30 @@
-module regfile (
+`define BITS
+
+module regfile #(
+    parameter `BITS N = 32
+) (
     input clk,
     input writeEnable,
     input [4:0] writeAddr,
-    input [31:0] writeData,
+    input [N - 1:0] writeData,
     input [4:0] readAddrA,
     input [4:0] readAddrB,
-    output [31:0] rdA,
-    output [31:0] rdB
+    output [N - 1:0] rdA,
+    output [N - 1:0] rdB
 );
+    reg [N - 1:0] regfile[31:0];
 
-  reg [31:0] regfile[31:0];
 
-  assign rdA = regfile[readAddrA];
-  assign rdB = regfile[readAddrB];
+    assign rdA = regfile[readAddrA];
+    assign rdB = regfile[readAddrB];
 
-  always @(posedge clk) begin
-    if (writeEnable) regfile[writeAddr] = writeData;
-  end
+    integer i;
+    always @(posedge clk) begin
+        if (writeEnable) regfile[writeAddr] = writeData;
+        // Set zero register to always be zero
+        for (i = 0; i < N; i = i + 1) begin 
+            regfile[0][i] <= 1'b0;
+        end
+    end
 
 endmodule
